6780553b9d9ce6890ea1c9de77d1327093055ceb
TwoCreepers <z15296529345@outlook.com>
2025-03-08 11:38:20 +0800

    更新 mod/searchOnlineInfo/__init__.py 文件 移除 __all__ 变量的 initialize

--- mod/searchOnlineInfo/__init__.py
+++ mod/searchOnlineInfo/__init__.py
@@ -5,7 +5,7 @@ from ._initialize import initialize
 from ._search._search import auto
 from ._bing._bing import bing
 
-__all__ = ['initialize', 'auto', 'bing']
+__all__ = ['auto', 'bing']
 
 __version__ = '0.1.0'
 __author__ = 'Carol Destiny'
