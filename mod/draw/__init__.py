e5d333b2d5d63644b44563b56c9512e0f3908adc
TwoCreepers <z15296529345@outlook.com>
2025-03-08 11:40:18 +0800

    renamed mod/draw/draw.py -> mod/draw/__init__.py 修复 draw 不是包而是模块的问题

--- mod/draw/__init__.py
+++ mod/draw/__init__.py
@@ -4,5 +4,10 @@ from ._initialize import initialize
 from ._group import group
 from ._private import private
 
+__all__ = ['group', 'private']
+
+__version__ = '0.1.0'
+__author__ = 'Carol Destiny'
+__doc__ = ''
 
 initialize()
