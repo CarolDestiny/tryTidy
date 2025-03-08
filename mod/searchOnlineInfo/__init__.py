7bf82de77153162f416f095ef988dd1ea2f40c79
TwoCreepers <z15296529345@outlook.com>
2025-03-08 11:35:55 +0800

    rename mod/searchOnlineInfo/searchOnlineInfo.py -> mod/searchOnlineInfo/__init__.py 修复 searchOnlineInfo 不是包而是模块的问题

---
+++ mod/searchOnlineInfo/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# flake8: noqa
+# 提供多种网络方法搜索信息
+from ._initialize import initialize
+from ._search._search import auto
+from ._bing._bing import bing
+
+__all__ = ['initialize', 'auto', 'bing']
+
+__version__ = '0.1.0'
+__author__ = 'Carol Destiny'
+__doc__ = '提供多种网络方法搜索信息'
+
+initialize()
--- mod/searchOnlineInfo/searchOnlineInfo.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-# flake8: noqa
-# searchOnlineInfo.py
-# 提供多种网络方法搜索信息
-from _initialize import initialize
-from ._search._search import auto
-from ._bing._bing import bing
-
-initialize()
